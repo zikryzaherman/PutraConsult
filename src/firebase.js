7d7fcadee920ea2f3d3b3f9cdccbeda83e420fe0
Zikry Zaherman <zikryaizuddin187@gmail.com>
2026-06-10 22:27:00 +0800

    refactor: update Firebase configuration and remove unused imports

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,21 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore"; 
-import { getAuth } from "firebase/auth";        
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "putraconsult.firebaseapp.com",
-  projectId: "putraconsult",
-  storageBucket: "putraconsult.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyCsP_T1_EnGFpWa6RVZPXzcMcfjXgrZXJA",
+  authDomain: "putraconsult-baddie888.firebaseapp.com",
+  projectId: "putraconsult-baddie888",
+  storageBucket: "putraconsult-baddie888.firebasestorage.app",
+  messagingSenderId: "862735939895",
+  appId: "1:862735939895:web:caa34ce62aec951c18c2b2",
+  measurementId: "G-8HS4H1P8R1"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-export const db = getFirestore(app);
-export const auth = getAuth(app);
+const analytics = getAnalytics(app);
